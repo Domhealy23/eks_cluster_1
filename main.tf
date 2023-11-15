79229c2f5469da04baa5191e7e75526f1c00b6eb
Dominic Healy <dominic.healy@corsearch.com>
2023-11-15 17:08:30 +0000

    eksctl config update

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+# variable "region" {}
+# variable "name" {}
+
+# module "vpc" {
+#   source = "./vpc"
+#   name   = var.name
+#   region = var.region
+# }
+
+
 variable "region" {}
 variable "name" {}
 
@@ -5,4 +15,14 @@ module "vpc" {
   source = "./vpc"
   name   = var.name
   region = var.region
+}
+
+module "config" {
+  source             = "./eksctl_config"
+  name               = var.name
+  region             = var.region
+  vpc_id             = module.vpc.vpc_id
+  instance_type      = "m5.2xlarge"
+  public_key_name    = "joaquin"
+  filename           = "${path.module}/cluster_config.yaml"
 }
